0ac61ec10620899bb8db01dfa053df67fac375d3
figo5530 <figooooo0221@gmail.com>
2021-04-16 19:47:09 +0800

    commit tree 59aa4a3f8d93a504b64548ae354004f6a75d4d49

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+let dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+    let number = dodger.style.left.replace("px", "")
+    let left = parseInt(number, 10)
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`
+    }
+}
+
+function moveDodgerRight() {
+    let number = dodger.style.left.replace("px", "")
+    let left = parseInt(number, 10)
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
